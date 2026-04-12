736fd9fbe6928ebaaad0fc41ae097b14dae7ddbb
Vignesh Rangarajan <68763948+VigRAN084@users.noreply.github.com>
2026-04-11 17:58:54 -0700

    Add: largest-rectangle-in-histogram - submission-1

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        '''
+        One Pass Solution:
+        - Iterate from left to right
+        - Maintain a 'start' index for each height as u pop
+        - it will give u the leftmost index while the nondecreasing 
+             invariant of the stack gives u the rightmost index 
+
+        Stack = [(start, height)]
+        '''
+
+        n = len(heights)
+        stack = [] 
+        maxArea = float('-inf')
+        for i, height in enumerate(heights):
+            # naively assume u can start the rectangle at height i
+            # it might be updated during popping to shift left
+            start = i
+            while stack and stack[-1][1] > height:
+                idx, h = stack.pop()      #idx == start for that height
+                start = idx
+                maxArea = max(maxArea, (i - idx) * h)
+                print(i, idx, h, maxArea)
+            # atp, u know the leftmost index for the current height
+            stack.append((start, height))
+        
+        # need to postprocess the leftover elements....
+        # these ones can stretch indefinitely towards the right b/c of 
+        # nondecreasing stack
+        for idx, height in stack:
+            maxArea = max(maxArea, (n - idx) * height)
+
+        return maxArea  
+
+
+
+
