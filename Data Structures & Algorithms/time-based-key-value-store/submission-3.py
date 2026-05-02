5dbe49ae98ce146b66eff4a44d95aa71f12522dc
Vignesh Rangarajan <68763948+VigRAN084@users.noreply.github.com>
2026-05-02 11:34:18 -0700

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,33 @@
+class TimeMap:
+
+    def __init__(self):
+        self.kv = defaultdict(list)
+
+    '''
+    Called in strictly increasing order
+    '''
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.kv[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.kv[key]
+        l = 0
+        r = len(values) - 1
+
+        # need to "prefer" the left half b/c we want the most recent value of key
+        while l <= r:
+            m = (l + r) // 2
+            print(m)
+            if values[m][1] == timestamp:
+                return values[m][0]
+            # exclude right-half of your search space
+            elif timestamp < values[m][1]:
+                r = m - 1
+            else:
+                l = m + 1
+        res = min(l,r)
+        if res < 0: 
+            return ''
+        return values[res][0]
+
+        
